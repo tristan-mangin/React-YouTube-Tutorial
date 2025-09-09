d34f0245ff300691249c109e4c9644627a4c06ea
Tristan Mangin <tw.mangin@gmail.com>
2025-09-08 21:28:38 -0400

    Passing Data Via Props
    up to 54:43

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,13 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-    return <div><ListGroup /></div>
+      let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities"/>
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,27 +2,37 @@
 
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
-	const [selectedIndex, setSelectedIndex] = useState(-1);
-	// Hook
-	// arr[0] --> variable (selectedIndex)
-	// arr[1] --> updater function
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-	// Event handler function
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // Hook
+  // arr[0] --> variable (selectedIndex)
+  // arr[1] --> updater function
+
+  // Event handler function
 
   return (
     // <> = FRAGMENT --> So that you can return multiple elements
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/* {items.length === 0 ? <p>No items found</p> : null}  --> same as below*/}
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => {setSelectedIndex(index)}}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li> // list items should have key
