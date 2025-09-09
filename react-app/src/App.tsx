addd12af6dd312c9f8772db5439f4ffc75029339
Tristan Mangin <tw.mangin@gmail.com>
2025-09-08 21:33:12 -0400

    Functions Via Props
    up to 58:29

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,15 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-      let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  }
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities"/>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectedItem} />
     </div>
   );
 }
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -5,9 +5,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   // Hook
   // arr[0] --> variable (selectedIndex)
@@ -32,6 +33,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
